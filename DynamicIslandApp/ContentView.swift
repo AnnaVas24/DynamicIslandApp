cc8282962682bc1b705515bfbca6a837af29b75d
AnnaVas24 <95534449+AnnaVas24@users.noreply.github.com>
2022-10-02 19:17:42 +0300

    dynamic island activity kit

--- DynamicIslandApp/ContentView.swift
+++ DynamicIslandApp/ContentView.swift
@@ -6,16 +6,40 @@
 //
 
 import SwiftUI
+import ActivityKit
 
 struct ContentView: View {
+    
+    @State private var currentActivity: Activity<TimerAttributes>?
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Image(systemName: "clock.fill")
+                .resizable()
+                .frame(width: 100, height: 100)
+                .foregroundColor(.gray)
+            Button(action: startActivity) {
+                Text("Start Timer")
+                    .foregroundColor(.red)
+                    .font(.title)
+            }
         }
         .padding()
+    }
+    
+    func startActivity(){
+        let startDate = Date.now
+        guard let endDate = Calendar.current.date(byAdding: .minute, value: 1, to: startDate) else { return }
+        let interval = startDate...endDate
+        
+        let attributes = TimerAttributes(actionName: "Cooking pasta")
+        let state = TimerAttributes.TimeState(plannedDuration: interval)
+        
+        do {
+            currentActivity = try Activity<TimerAttributes>.request(attributes: attributes, contentState: state)
+        } catch {
+            print(error.localizedDescription)
+        }
     }
 }
 
---
+++ LiveActivityAttributes.swift
@@ -0,0 +1,20 @@
+//
+//  LiveActivityAttributes.swift
+//  DynamicIslandApp
+//
+//  Created by Vasichko Anna on 02.10.2022.
+//
+
+import Foundation
+import ActivityKit
+
+public struct TimerAttributes: ActivityAttributes {
+    
+    public typealias TimeState = ContentState
+    
+    public struct ContentState: Codable, Hashable {
+        var plannedDuration: ClosedRange<Date>
+    }
+    
+    var actionName: String
+}
